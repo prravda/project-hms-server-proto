38eb16bf90f096470e7d5a9ba39c930dc04bddc7
InseobJeon <sonofhuman20@gmail.com>
2021-07-26 23:08:27 +0900

    Feature: Add pino as a logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['verbose'],
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
 }
